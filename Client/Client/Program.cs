9b003ef0fa4bb34c2b0d250dcb88c0a044f62ab8
Tangyu Fu <futangyu@gmail.com>
2021-12-01 17:53:27 +0800

    初始化仓库
    添加客户端和服务端代码

---
+++ Client/Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = null;
+            try
+            {
+                tcpClient = new TcpClient("127.0.0.1", 13000);
+                NetworkStream networkStream = tcpClient.GetStream();
+                Byte[] header = new Byte[2];
+                Byte[] packet = new Byte[1024 * 8];
+                MemoryStream headerMemoryStream = new MemoryStream(header);
+                BinaryWriter headerBinaryWriter = new BinaryWriter(headerMemoryStream);
+                BinaryReader headerBinaryReader = new BinaryReader(headerMemoryStream);
+                string input;
+                // Send console input string.
+                while ((input = Console.ReadLine()) != "")
+                {
+                    ushort packetLen = (ushort) Encoding.ASCII.GetBytes(input, packet);
+                    headerMemoryStream.Position = 0L;
+                    // Use BinaryWriter for little-endian, not BitConverter.
+                    headerBinaryWriter.Write(packetLen);
+                    // Write 2 byte for packet length.
+                    networkStream.Write(header);
+                    // Write packet.
+                    networkStream.Write(packet, 0, packetLen);
+                    Console.WriteLine("Sent: header {0}, packet {1}", BitConverter.ToString(header), BitConverter.ToString(packet, 0, packetLen));
+                    
+                    int offset = 0;
+                    int count;
+                    // Read header, 2 bytes for packet length.
+                    while ((count = networkStream.Read(header, offset, header.Length - offset)) != 0)
+                    {
+                        offset += count;
+                        if (offset < header.Length)
+                        {
+                            continue;
+                        }
+                    
+                        headerMemoryStream.Position = 0L;
+                        packetLen = headerBinaryReader.ReadUInt16();
+                        Console.WriteLine("Received header: {0} packet len {1}", BitConverter.ToString(header),
+                            packetLen);
+                        offset = 0;
+                        // Read packet, packet length is packetLen.
+                        while ((count = networkStream.Read(packet, offset, packetLen - offset)) != 0)
+                        {
+                            offset += count;
+                            if (offset < packetLen)
+                            {
+                                continue;
+                            }
+
+                            Console.WriteLine("Received packet: {0}", BitConverter.ToString(packet, 0, packetLen));
+                            // Break for next input.
+                            break;
+                        }
+                        // Break for next input.
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TcpClient exception: {0}", e);
+            }
+            finally
+            {
+                tcpClient?.Close();
+            }
+            
+            Console.WriteLine("Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                server = new TcpListener(IPAddress.Parse("127.0.0.1"), 13000);
+                server.Start();
+
+                Byte[] header = new Byte[2];
+                MemoryStream headerMemoryStream = new MemoryStream(header);
+                BinaryReader headerBinaryReader = new BinaryReader(headerMemoryStream);
+                Byte[] packet = new Byte[1024 * 8];
+                while (true)
+                {
+                    TcpClient client = null;
+                    try
+                    {
+                        Console.WriteLine("Waiting for a connection... ");
+                        client = server.AcceptTcpClient();
+                        Console.WriteLine("Connected!");
+                        NetworkStream networkStream = client.GetStream();
+                        int offset = 0;
+                        int count;
+                        // Read header, 2 bytes for packet length.
+                        while ((count = networkStream.Read(header, offset, header.Length - offset)) != 0)
+                        {
+                            offset += count;
+                            if (offset < header.Length)
+                            {
+                                continue;
+                            }
+
+                            headerMemoryStream.Position = 0L;
+                            ushort packetLen = headerBinaryReader.ReadUInt16();
+                            Console.WriteLine("Received header: {0} packet len {1}", BitConverter.ToString(header),
+                                packetLen);
+                            offset = 0;
+                            // Read packet, packet length is packetLen.
+                            while ((count = networkStream.Read(packet, offset, packetLen - offset)) != 0)
+                            {
+                                offset += count;
+                                if (offset < packetLen)
+                                {
+                                    continue;
+                                }
+
+                                Console.WriteLine("Received packet: {0}", BitConverter.ToString(packet, 0, packetLen));
+                                networkStream.Write(header);
+                                networkStream.Write(packet, 0, packetLen);
+                                Console.WriteLine("Sent: header {0}, packet {1}", BitConverter.ToString(header),
+                                    BitConverter.ToString(packet, 0, packetLen));
+                            }
+
+                            offset = 0;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("TcpClient exception: {0}", e);
+                    }
+                    finally
+                    {
+                        // Shutdown and end connection
+                        client?.Close();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TcpListener exception: {0}", e);
+            }
+            finally
+            {
+                server?.Stop();
+            }
+
+            Console.WriteLine("Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
